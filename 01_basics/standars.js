e1747a6051ccd2925a09b8714df998c6270e0d31
Abhishek kaushik <66107155+abhishekkaushik123@users.noreply.github.com>
2025-06-27 17:52:12 +0000

    commit

---
+++ 01_basics/standars.js
@@ -0,0 +1,27 @@
+"use strict"// its basically inform to the javascript engine that we use new version syntax
+
+console(3+
+    3
+)  //take care about the rediablity of the programe
+
+//for the documentation refer to the mdn/tc39
+
+//datatypes
+let a="abhishek";
+let b=123;
+let c=123.34;
+let d=true;
+
+
+//another
+//null->that basically point to empty
+//undefined->which is declared but not defined
+//symbol->that is use for the uniqueness
+//number
+//boolean
+//string=""
+
+//objects
+
+console.log(typeof "abhishek");
+console.log(typeof null);
---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+//for the constant we use const keyword
+const myName="abhishek kaushik";
+
+//for variable we have 2 option's->let[have scope boundaries]/var[doesn't support scope boundaries]
+var myMarks=123;
+
+let myVillage="kallarpur rajput";
+
+console.log(myMarks);
+console.log(myName);
+console.log(myVillage);
+
+//another way to print all variables once using console.table([])
+console.table([myMarks,myName,myVillage]);
