d22c85c527f0d76ea9a6bfa5e8e4b6af620b863a
Abhishek kaushik <66107155+abhishekkaushik123@users.noreply.github.com>
2025-06-28 06:38:24 +0000

    string commit

---
+++ 01_basics/string.js
@@ -0,0 +1,10 @@
+//use `` to inject variables with string
+let myName="Abhishek Kaushik";
+let myAge=25;
+
+console.log(`my name is ${myName} and my age is: ${myAge}`);
+
+//their are 2 ways to declare the string
+let myAddress="kallarpur"
+let myAddress1=new String("Kallarpur")
+
